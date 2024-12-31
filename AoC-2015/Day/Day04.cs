24403f8a152930b03b82f30557c5dcc824851f9e
jas2o <43341263+Jas2o@users.noreply.github.com>
2024-12-31 14:13:59 +1100

    2016: Day 5

--- AoC-2015/Day/Day04.cs
+++ AoC-2015/Day/Day04.cs
@@ -32,7 +32,7 @@
             //Answer: 9962624
         }
 
-        public static string CreateMD5(string input) {
+        private static string CreateMD5(string input) {
             // Use input string to calculate MD5 hash
             using (System.Security.Cryptography.MD5 md5 = System.Security.Cryptography.MD5.Create()) {
                 byte[] inputBytes = System.Text.Encoding.ASCII.GetBytes(input);
--- AoC-2016/Day/Day05.cs
+++ AoC-2016/Day/Day05.cs
@@ -1,18 +1,56 @@
-﻿using System;
-using System.Globalization;
-using System.Text;
+﻿using System.Text;
 
-namespace AoC.Day
-{
+namespace AoC.Day {
     public class Day05
     {
         public static void Run(string file) {
-            Console.WriteLine("Day 5: ???" + Environment.NewLine);
+            Console.WriteLine("Day 5: How About a Nice Game of Chess?" + Environment.NewLine);
 
-            Console.WriteLine("Part 1: " + 0);
-            //Answer: 
-            Console.WriteLine("Part 2: " + 0);
-            //Answer: 
+            string input = File.ReadAllText(file);
+            
+            int passwordLength = 8;
+            StringBuilder passA = new StringBuilder();
+            char[] passB = new char[passwordLength];
+
+            int num = 0;
+            while (true) {
+                string secret = num.ToString();
+                string output = CreateMD5(input + secret);
+
+                if (output.Substring(0, 5) == "00000") {
+                    Console.WriteLine(output);
+                    if(passA.Length < passwordLength)
+                        passA.Append(output[5]);
+
+                    if(int.TryParse(output.Substring(5, 1), out int pos)) {
+                        if(pos < passwordLength && passB[pos] == 0) {
+                            passB[pos] = output[6];
+                        }
+                    }
+                }
+
+                if (passA.Length == passwordLength && !passB.Contains((char)0))
+                    break;
+                num++;
+            }
+
+            string partA = passA.ToString().ToLower();
+            string partB = new string(passB).ToLower();
+
+            Console.WriteLine();
+            Console.WriteLine("Part 1: " + partA);
+            //Answer: c6697b55
+            Console.WriteLine("Part 2: " + partB);
+            //Answer: 8c35d1ab
+        }
+
+        private static string CreateMD5(string input) {
+            using (System.Security.Cryptography.MD5 md5 = System.Security.Cryptography.MD5.Create()) {
+                byte[] inputBytes = Encoding.ASCII.GetBytes(input);
+                byte[] hashBytes = md5.ComputeHash(inputBytes);
+
+                return Convert.ToHexString(hashBytes);
+            }
         }
     }
 }
