429c0de70b1b92c1a2122abc4b458e134a97dc0c
jas2o <43341263+Jas2o@users.noreply.github.com>
2024-12-31 14:28:36 +1100

    2016: Day 6

--- AoC-2016/Day/Day06.cs
+++ AoC-2016/Day/Day06.cs
@@ -1,18 +1,37 @@
-﻿using System;
-using System.Globalization;
-using System.Text;
+﻿using System.Text;
 
-namespace AoC.Day
-{
+namespace AoC.Day {
     public class Day06
     {
         public static void Run(string file) {
-            Console.WriteLine("Day 6: ???" + Environment.NewLine);
+            Console.WriteLine("Day 6: Signals and Noise" + Environment.NewLine);
 
-            Console.WriteLine("Part 1: " + 0);
-            //Answer: 
-            Console.WriteLine("Part 2: " + 0);
-            //Answer: 
+            string[] lines = File.ReadAllLines(file);
+
+            StringBuilder sbA = new StringBuilder();
+            StringBuilder sbB = new StringBuilder();
+            Dictionary<(int, char), int> dictionary = new Dictionary<(int, char), int>();
+            for (int pos = 0; pos < lines[0].Length; pos++) {
+                foreach (string line in lines) {
+                    char c = line[pos];
+                    if (dictionary.ContainsKey((pos, c))) {
+                        dictionary[(pos, c)]++;
+                    } else {
+                        dictionary.Add((pos, c), 1);
+                    }
+                }
+
+                char most = dictionary.Where(x => x.Key.Item1 == pos).MaxBy(x => x.Value).Key.Item2;
+                sbA.Append(most);
+
+                char least = dictionary.Where(x => x.Key.Item1 == pos).MinBy(x => x.Value).Key.Item2;
+                sbB.Append(least);
+            }
+
+            Console.WriteLine("Part 1: " + sbA.ToString());
+            //Answer: mshjnduc
+            Console.WriteLine("Part 2: " + sbB.ToString());
+            //Answer: apfeeebz
         }
     }
 }
