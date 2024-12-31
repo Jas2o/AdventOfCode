d87ab7617df2a2a3d6a5b03f8ac7de5fda2d0870
jas2o <43341263+Jas2o@users.noreply.github.com>
2024-12-31 13:49:40 +1100

    2016: Day 4

--- AoC-2016/Day/Day04.cs
+++ AoC-2016/Day/Day04.cs
@@ -1,18 +1,80 @@
-﻿using System;
-using System.Globalization;
-using System.Text;
+﻿using System.Text;
 
 namespace AoC.Day
 {
     public class Day04
     {
-        public static void Run(string file) {
-            Console.WriteLine("Day 14: ???" + Environment.NewLine);
+        private static string alphabet = "abcdefghijklmnopqrstuvwxyz";
 
-            Console.WriteLine("Part 1: " + 0);
-            //Answer: 
-            Console.WriteLine("Part 2: " + 0);
-            //Answer: 
+        public static void Run(string file) {
+            Console.WriteLine("Day 4: Security Through Obscurity" + Environment.NewLine);
+
+            int sum = 0;
+            int north = 0;
+            string[] lines = File.ReadAllLines(file);
+            foreach(string line in lines) {
+                int sector = CheckThis(line, out string name);
+                if (sector > 0) {
+                    sum += sector;
+                    StringBuilder sb = new StringBuilder();
+                    foreach(char c in name) {
+                        if (c == '-')
+                            sb.Append(' ');
+                        else {
+                            int num = (((c - 'a') + sector) % 26) + 'a';
+                            sb.Append((char)num);
+                        }
+                    }
+                    string nameDecrypted = sb.ToString();
+                    if (nameDecrypted.Contains("north")) {
+                        north = sector;
+                        Console.ForegroundColor = ConsoleColor.Green;
+                    }
+                    Console.WriteLine("[{0}] is [{1}]", name, nameDecrypted);
+                    Console.ResetColor();
+                }
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Part 1: " + sum);
+            //Answer: 185371
+            Console.WriteLine("Part 2: " + north);
+            //Answer: 984
+        }
+
+        private static int CheckThis(string line, out string name) {
+            int indexSquare = line.IndexOf('[');
+            string checksum = line.Substring(indexSquare + 1, 5);
+            name = line.Substring(0, indexSquare);
+            int indexLastDash = line.LastIndexOf('-');
+            int sector = int.Parse(name.Substring(indexLastDash + 1));
+            name = name.Substring(0, indexLastDash);
+
+            //Console.WriteLine("{0} // {1} // {2}", name, sector, checksum);
+
+            Dictionary<char, int> check = new Dictionary<char, int>();
+            foreach (char c in alphabet) {
+                int count = name.Count(x => x == c);
+                if(count > 0)
+                    check.Add(c, count);
+            }
+
+            KeyValuePair<char,int>[] checks = check.OrderByDescending(x => x.Value).ToArray();
+            //foreach(KeyValuePair<char, int> pair in checks) {
+                //Console.WriteLine("CHECK {0} was {1} times", pair.Key, pair.Value);
+            //}
+
+            for(int i = 0; i < 5; i++) {
+                char c = checksum[i];
+                int count = name.Count(x => x == c);
+                
+                //Console.WriteLine("      {0} was {1} times", c, count);
+
+                if (checks[i].Key != c)
+                    return 0;
+            }
+
+            return sector;
         }
     }
 }
