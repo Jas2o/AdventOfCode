90a8f0ed3476eeb0873481ab721765f0f08c7a7a
jas2o <43341263+Jas2o@users.noreply.github.com>
2025-01-05 23:42:50 +1100

    2016: Day 16

--- AoC-2016/Day/Day16.cs
+++ AoC-2016/Day/Day16.cs
@@ -1,18 +1,65 @@
-﻿using System;
-using System.Globalization;
-using System.Text;
+﻿using System.Text;
 
 namespace AoC.Day
 {
     public class Day16
     {
         public static void Run(string file) {
-            Console.WriteLine("Day 16: ???" + Environment.NewLine);
+            Console.WriteLine("Day 16: Dragon Checksum" + Environment.NewLine);
 
-            Console.WriteLine("Part 1: " + 0);
-            //Answer: 
-            Console.WriteLine("Part 2: " + 0);
-            //Answer: 
+            string input = File.ReadAllText(file);
+            int diskLen = 272;
+            if (input == "10000")
+                diskLen = 20;
+
+            string filled = GetFilled(input, diskLen);
+            string checksum = GetChecksum(filled);
+
+            string partB = GetChecksum(GetFilled(input, 35651584));
+
+            Console.WriteLine("Part 1: " + checksum.ToString());
+            //Answer: 00000100100001100
+            Console.WriteLine("Part 2: " + partB);
+            //Answer: 00011010100010010
+        }
+
+        private static string GetFilled(string input, int length) {
+            StringBuilder sb = new StringBuilder();
+            sb.Append(input);
+            
+            while (sb.Length < length) {
+                string a = sb.ToString();
+                StringBuilder b = new StringBuilder();
+                for (int i = a.Length - 1; i >= 0; i--) {
+                    if (a[i] == '0')
+                        b.Append('1');
+                    else
+                        b.Append('0');
+                }
+                string res = "0" + b.ToString();
+                sb.Append(res);
+            }
+
+            return sb.ToString().Substring(0, length);
+        }
+
+        private static string GetChecksum(string filled) {
+            string checksum = filled;
+            do {
+                StringBuilder cs = new StringBuilder();
+                for (int i = 1; i < checksum.Length; i += 2) {
+                    char left = checksum[i - 1];
+                    char right = checksum[i];
+
+                    if (left == right)
+                        cs.Append("1");
+                    else
+                        cs.Append("0");
+                }
+                checksum = cs.ToString();
+            } while (checksum.Length % 2 == 0);
+
+            return checksum;
         }
     }
 }
