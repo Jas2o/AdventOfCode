6bdb6d321f33c8a177dba90edc44bb8b69c3e89f
jas2o <43341263+Jas2o@users.noreply.github.com>
2025-01-06 12:50:15 +1100

    2016: Day 19

--- AoC-2016/Day/Day19.cs
+++ AoC-2016/Day/Day19.cs
@@ -1,18 +1,69 @@
-﻿using System;
-using System.Globalization;
-using System.Text;
-
-namespace AoC.Day
+﻿namespace AoC.Day
 {
     public class Day19
     {
-        public static void Run(string file) {
-            Console.WriteLine("Day 19: ???" + Environment.NewLine);
+        // Thanks to u/Smylers as I was not willing to put in effort to solve this.
 
-            Console.WriteLine("Part 1: " + 0);
-            //Answer: 
-            Console.WriteLine("Part 2: " + 0);
-            //Answer: 
+        public static void Run(string file) {
+            Console.WriteLine("Day 19: An Elephant Named Joseph" + Environment.NewLine);
+
+            int last = int.Parse(File.ReadAllText(file));
+
+            int partA = SolveA_Fast(last);
+            int partB = SolveB(last);
+
+            Console.WriteLine("Part 1: " + partA);
+            //Answer: 1815603
+            Console.WriteLine("Part 2: " + partB);
+            //Answer: 1410630
+        }
+
+        private static int SolveA(int last) {
+            Queue<int> queue = new Queue<int>();
+            for (int i = 1; i <= last; i++) {
+                queue.Enqueue(i);
+            }
+
+            while (queue.Count > 0) {
+                int stealer = queue.Dequeue();
+                if (queue.Count == 0)
+                    return stealer;
+                int loser = queue.Dequeue();
+                queue.Enqueue(stealer);
+            }
+            return 0;
+        }
+
+        private static int SolveA_Fast(int last) {
+            //Josephus problem using binary.
+            string binary = Convert.ToString(last, 2);
+            binary = binary.Substring(1) + binary[0];
+            int winner = Convert.ToInt32(binary, 2);
+            return winner;
+        }
+
+        private static int SolveB(int last) {
+            Queue<int> queue1 = new Queue<int>();
+            Queue<int> queue2 = new Queue<int>();
+            int mid = last / 2;
+            for (int i = 1; i <= mid; i++)
+                queue1.Enqueue(i);
+            for (int i = mid + 1; i <= last; i++)
+                queue2.Enqueue(i);
+
+            while (queue1.Count > 0) {
+                int loser = queue2.Dequeue();
+
+                if (queue1.Count == queue2.Count) {
+                    int move = queue2.Dequeue();
+                    queue1.Enqueue(move);
+                }
+
+                int stealer = queue1.Dequeue();
+                queue2.Enqueue(stealer);
+            }
+
+            return queue2.Dequeue();
         }
     }
 }
