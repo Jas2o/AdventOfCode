9f448b52c421c2e4fc4148bf44a690730f7c086a
jas2o <43341263+Jas2o@users.noreply.github.com>
2025-01-08 02:06:17 +1100

    2017: Day 2

--- AoC-2017/Day/Day02.cs
+++ AoC-2017/Day/Day02.cs
@@ -3,16 +3,38 @@
     public class Day02
     {
         public static void Run(string file) {
-            Console.WriteLine("Day 2: ???" + Environment.NewLine);
+            Console.WriteLine("Day 2: Corruption Checksum" + Environment.NewLine);
 
-			string input = File.ReadAllText(file);
+            int partA = 0;
+            int partB = 0;
+
 			string[] lines = File.ReadAllLines(file);
+            foreach (string line in lines) {
+                int[] num = Array.ConvertAll(line.Replace("\t", " ").Split(' '), int.Parse);
 
-			Console.WriteLine();
-            Console.WriteLine("Part 1: " + 0);
-            //Answer: 
-            Console.WriteLine("Part 2: " + 0);
-            //Answer: 
+                partA += num.Max() - num.Min();
+
+                for(int x = 0; x < num.Length; x++) {
+                    for (int y = 0; y < num.Length; y++) {
+                        if (x == y)
+                            continue;
+                        int left = num[x];
+                        int right = num[y];
+                        if (right == 0)
+                            continue;
+                        int mod = left % right;
+                        if(mod == 0) {
+                            partB += left / right;
+                            break;
+                        }
+                    }
+                }
+            }
+
+            Console.WriteLine("Part 1: " + partA);
+            //Answer: 50376
+            Console.WriteLine("Part 2: " + partB);
+            //Answer: 267
         }
     }
 }
