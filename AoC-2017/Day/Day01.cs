58c215757147c203fc959fc96c5cb3a9c59fa438
jas2o <43341263+Jas2o@users.noreply.github.com>
2025-01-08 01:11:35 +1100

    2017: Day 1

--- AoC-2017/Day/Day01.cs
+++ AoC-2017/Day/Day01.cs
@@ -3,16 +3,39 @@
     public class Day01
     {
         public static void Run(string file) {
-            Console.WriteLine("Day 1: ???" + Environment.NewLine);
+            Console.WriteLine("Day 1: Inverse Captcha" + Environment.NewLine);
 
-			string input = File.ReadAllText(file);
 			string[] lines = File.ReadAllLines(file);
+            foreach (string line in lines) {
+                //There's only one line in the real input.
+                Console.ForegroundColor = ConsoleColor.DarkGray;
+                Console.WriteLine(line);
+                Console.ResetColor();
 
-			Console.WriteLine();
-            Console.WriteLine("Part 1: " + 0);
-            //Answer: 
-            Console.WriteLine("Part 2: " + 0);
-            //Answer: 
+                int sumA = 0;
+                int sumB = 0;
+                int offset = (line.Length / 2);
+                int prev = (int)char.GetNumericValue(line.Last());
+                for (int i = 0; i < line.Length; i++) {
+                    int current = (int)char.GetNumericValue(line[i]);
+                    int other = (int)char.GetNumericValue(line[(i + offset) % line.Length]);
+
+                    if (current == prev)
+                        sumA += current;
+                    if (current == other)
+                        sumB += current;
+
+                    prev = current;
+                }
+
+                Console.WriteLine();
+                Console.WriteLine("Part 1: " + sumA);
+                //Answer: 1171
+                Console.WriteLine("Part 2: " + sumB);
+                //Answer: 1024
+
+                Console.WriteLine();
+            }
         }
     }
 }
