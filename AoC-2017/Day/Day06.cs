b3ef7f4af370e898a57444920e4c8549c169b7e8
jas2o <43341263+Jas2o@users.noreply.github.com>
2025-01-08 18:37:02 +1100

    2017: Day 6

--- AoC-2017/Day/Day06.cs
+++ AoC-2017/Day/Day06.cs
@@ -3,16 +3,45 @@
     public class Day06
     {
         public static void Run(string file) {
-            Console.WriteLine("Day 6: ???" + Environment.NewLine);
+            Console.WriteLine("Day 6: Memory Reallocation" + Environment.NewLine);
 
-			string input = File.ReadAllText(file);
-			string[] lines = File.ReadAllLines(file);
+            string input = File.ReadAllText(file);
+            int[] num = Array.ConvertAll(input.Split("\t"), int.Parse);
 
-			Console.WriteLine();
-            Console.WriteLine("Part 1: " + 0);
-            //Answer: 
-            Console.WriteLine("Part 2: " + 0);
-            //Answer: 
+            List<string> seen = new List<string>();
+            seen.Add(string.Join(',', num));
+
+            int steps = 0;
+            int steps2 = 0;
+            string alreadySeen = string.Empty;
+            while (true) {
+                int max = num.Max();
+                int maxIndex = Array.IndexOf(num, max);
+                num[maxIndex] = 0;
+                for(int i = 1; i <= max; i++) {
+                    int pos = (maxIndex + i) % num.Length;
+                    num[pos]++;
+                }
+
+                string test = string.Join(',', num);
+                if (alreadySeen == string.Empty) {
+                    steps++;
+                    if (seen.Contains(test)) {
+                        alreadySeen = test;
+                    }
+                } else {
+                    steps2++;
+                    if (test == alreadySeen) {
+                        break;
+                    }
+                }
+                seen.Add(test);
+            }
+
+            Console.WriteLine("Part 1: " + steps);
+            //Answer: 6681
+            Console.WriteLine("Part 2: " + steps2);
+            //Answer: 2392
         }
     }
 }
