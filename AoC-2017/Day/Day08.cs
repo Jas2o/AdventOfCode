294dc69c9c47f514e4705b045c1829d8c8cd6077
jas2o <43341263+Jas2o@users.noreply.github.com>
2025-01-09 13:03:42 +1100

    2017: Day 8

--- AoC-2017/Day/Day08.cs
+++ AoC-2017/Day/Day08.cs
@@ -3,16 +3,55 @@
     public class Day08
     {
         public static void Run(string file) {
-            Console.WriteLine("Day 8: ???" + Environment.NewLine);
+            Console.WriteLine("Day 8: I Heard You Like Registers" + Environment.NewLine);
 
-			string input = File.ReadAllText(file);
+            Dictionary<string, int> dRegister = new Dictionary<string, int>();
+
 			string[] lines = File.ReadAllLines(file);
+            int highest = 0;
+            foreach (string line in lines) {
+                string[] fields = line.Split(' ');
 
-			Console.WriteLine();
-            Console.WriteLine("Part 1: " + 0);
-            //Answer: 
-            Console.WriteLine("Part 2: " + 0);
-            //Answer: 
+                string left = fields[0];
+                string leftOp = fields[1];
+                int leftNum = int.Parse(fields[2]);
+                if (!dRegister.ContainsKey(left))
+                    dRegister.Add(left, 0);
+
+                string right = fields[4];
+                string rightOp = fields[5];
+                int rightNum = int.Parse(fields[6]);
+                if (!dRegister.ContainsKey(right))
+                    dRegister.Add(right, 0);
+
+                bool check = false;
+                switch (rightOp) {
+                    case ">": check = dRegister[right] > rightNum; break;
+                    case "<": check = dRegister[right] < rightNum; break;
+                    case ">=": check = dRegister[right] >= rightNum; break;
+                    case "<=": check = dRegister[right] <= rightNum; break;
+                    case "==": check = dRegister[right] == rightNum; break;
+                    case "!=": check = dRegister[right] != rightNum; break;
+                    default:
+                        throw new Exception();
+                }
+
+                if (check) {
+                    if (leftOp == "inc")
+                        dRegister[left] += leftNum;
+                    else //dec
+                        dRegister[left] -= leftNum;
+                }
+
+                highest = Math.Max(highest, dRegister[left]);
+            }
+
+            int largest = dRegister.Max(r => r.Value);
+
+            Console.WriteLine("Part 1: " + largest);
+            //Answer: 4567
+            Console.WriteLine("Part 2: " + highest);
+            //Answer: 5636
         }
     }
 }
