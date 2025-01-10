dd282e60c52247339f9cf3ca18c581ce6f5ac186
jas2o <43341263+Jas2o@users.noreply.github.com>
2025-01-10 11:40:39 +1100

    2017: Day 11

--- AoC-2017/Day/Day11.cs
+++ AoC-2017/Day/Day11.cs
@@ -3,16 +3,42 @@
     public class Day11
     {
         public static void Run(string file) {
-            Console.WriteLine("Day 11: ???" + Environment.NewLine);
+            Console.WriteLine("Day 11: Hex Ed" + Environment.NewLine);
 
-			string input = File.ReadAllText(file);
 			string[] lines = File.ReadAllLines(file);
+            foreach (string line in lines) {
+                //There's only one line in the real input.
+                //Console.WriteLine(line);
 
-			Console.WriteLine();
-            Console.WriteLine("Part 1: " + 0);
-            //Answer: 
-            Console.WriteLine("Part 2: " + 0);
-            //Answer: 
+                string[] path = line.Split(',');
+
+                //Cube coordinates
+                int q = 0; 
+                int r = 0;
+                int s = 0;
+                int furthest = 0;
+                foreach (string p in path) {
+                    switch(p) {
+                        case "n": s++; r--;  break;
+                        case "ne": q++; r--; break;
+                        case "nw": q--; s++; break;
+                        case "sw": q--; r++; break;
+                        case "se": q++; s--; break;
+                        case "s": s--; r++; break;
+                    }
+
+                    int d = (Math.Abs(q) + Math.Abs(r) + Math.Abs(s)) / 2;
+                    furthest = Math.Max(furthest, d);
+                }
+
+                int distance = (Math.Abs(q) + Math.Abs(r) + Math.Abs(s)) / 2;
+
+                Console.WriteLine("Part 1: " + distance);
+                //Answer: 818
+                Console.WriteLine("Part 2: " + furthest);
+                //Answer: 1596
+                Console.WriteLine();
+            }
         }
     }
 }
