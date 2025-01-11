a9bd5c854708b7f5b6f1403b99ff55da856a4ad5
jas2o <43341263+Jas2o@users.noreply.github.com>
2025-01-11 23:45:17 +1100

    2017: Day 17

--- AoC-2017/Day/Day17.cs
+++ AoC-2017/Day/Day17.cs
@@ -3,16 +3,35 @@
     public class Day17
     {
         public static void Run(string file) {
-            Console.WriteLine("Day 17: ???" + Environment.NewLine);
+            Console.WriteLine("Day 17: Spinlock" + Environment.NewLine);
 
 			string input = File.ReadAllText(file);
-			string[] lines = File.ReadAllLines(file);
+			int spinsPerInsert = int.Parse(input);
 
-			Console.WriteLine();
-            Console.WriteLine("Part 1: " + 0);
-            //Answer: 
-            Console.WriteLine("Part 2: " + 0);
-            //Answer: 
+            List<int> buffer = new List<int>() { 0 };
+            int pos = 0;
+
+            int numOfInsertions = 2017;
+            for (int i = 1; i <= numOfInsertions; i++) {
+                pos = ((pos + spinsPerInsert) % i) + 1;
+                buffer.Insert(pos, i);
+            }
+            int indexLastInsert = buffer.IndexOf(numOfInsertions);
+            int part1 = buffer[indexLastInsert + 1];
+
+            int numOfInsertions2 = 50000000;
+            int part2 = 0;
+            for (int i = numOfInsertions + 1; i <= numOfInsertions2; i++) {
+                pos = ((pos + spinsPerInsert) % i) + 1;
+                if (pos == 1)
+                    part2 = i;
+                //Since 0 is always in the first position, we just need to remember what was put after it.
+            }
+
+            Console.WriteLine("Part 1: " + part1);
+            //Answer: 808
+            Console.WriteLine("Part 2: " + part2);
+            //Answer: 47465686
         }
     }
 }
