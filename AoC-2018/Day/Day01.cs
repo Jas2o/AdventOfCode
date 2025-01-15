fa98695c2cc7d16f4738057043e0d11e246f4077
jas2o <43341263+Jas2o@users.noreply.github.com>
2025-01-15 11:04:03 +1100

    2018: Day 1

--- AoC-2018/Day/Day01.cs
+++ AoC-2018/Day/Day01.cs
@@ -3,16 +3,31 @@
     public class Day01
     {
         public static void Run(string file) {
-            Console.WriteLine("Day 1: ???" + Environment.NewLine);
+            Console.WriteLine("Day 1: Chronal Calibration" + Environment.NewLine);
 
-			string input = File.ReadAllText(file);
 			string[] lines = File.ReadAllLines(file);
+            int[] nums = Array.ConvertAll(lines, int.Parse);
 
-			Console.WriteLine();
-            Console.WriteLine("Part 1: " + 0);
-            //Answer: 
-            Console.WriteLine("Part 2: " + 0);
-            //Answer: 
+            int partA = nums.Sum();
+
+            int partB = 0;
+            int freq = 0;
+            Dictionary<int, int> dictionary = new Dictionary<int, int>() { { freq, -1 } };
+            int n = 0;
+            while (true) {
+                freq += nums[n % nums.Length];
+                if (dictionary.ContainsKey(freq)) {
+                    partB = freq;
+                    break;
+                }
+                dictionary.Add(freq, n);
+                n++;
+            }
+
+            Console.WriteLine("Part 1: " + partA);
+            //Answer: 574
+            Console.WriteLine("Part 2: {0} (after {1} changes)", partB, n);
+            //Answer: 452
         }
     }
 }
