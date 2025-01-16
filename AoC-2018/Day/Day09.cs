420bda625c0a30e2c84074c19f42719bd9bd0d1e
jas2o <43341263+Jas2o@users.noreply.github.com>
2025-01-16 21:34:35 +1100

    2018: Day 9 (part 2)

--- AoC-2018/Day/Day09.cs
+++ AoC-2018/Day/Day09.cs
@@ -1,4 +1,7 @@
-﻿namespace AoC.Day
+﻿using System.Text;
+using static AoC.Day.Day09;
+
+namespace AoC.Day
 {
     public class Day09
     {
@@ -12,29 +15,30 @@
                 string[] fields = line.Split(' ');
                 int numPlayers = int.Parse(fields[0]);
                 int highMarble = int.Parse(fields[6]);
-                int partA = Solve(numPlayers, highMarble);
-                int partB = Solve(numPlayers, highMarble * 100);
+                long partA = Solve(numPlayers, highMarble);
+                long partB = Solve(numPlayers, highMarble * 100);
 
                 Console.WriteLine("Part 1: " + partA);
                 //Answer: 398242
                 Console.WriteLine("Part 2: " + partB);
-                //Answer: 
+                //Answer: 3273842452
                 Console.WriteLine();
             }
         }
 
-        private static int Solve(int numPlayers, int highMarble) {
+        private static long Solve(int numPlayers, int highMarble) {
             bool verbose = (highMarble < 30);
 
-            List<int> circle = new List<int>();
             Queue<int> marbles = new Queue<int>(Enumerable.Range(1, highMarble));
-            Dictionary<int, List<int>> players = new Dictionary<int, List<int>>();
+            Dictionary<int, List<long>> players = new Dictionary<int, List<long>>();
             for (int p = 1; p <= numPlayers; p++) {
-                players[p] = new List<int>();
+                players[p] = new List<long>();
             }
 
-            circle.Add(0);
-            int pos = 0;
+            DLNode<int> root = new DLNode<int>(0);
+            root.Previous = root;
+            root.Next = root;
+            DLNode<int> current = root;
 
             while (marbles.Any()) {
                 for (int p = 1; p <= numPlayers; p++) {
@@ -44,68 +48,78 @@
                     int marble = marbles.Dequeue();
                     if (marble % 23 == 0) {
                         players[p].Add(marble);
-                        int takeFrom = pos - 7 % circle.Count;
+                        DLNode<int> takeMarble = current.Previous.Previous.Previous.Previous.Previous.Previous.Previous;
+                        players[p].Add(takeMarble.Value);
 
-                        if (takeFrom < 0)
-                            takeFrom += circle.Count;
+                        DLNode<int> prev = takeMarble.Previous;
+                        current = takeMarble.Next;
+                        prev.Next = current;
+                        current.Previous = prev;
 
-                        int marble2 = circle[takeFrom];
-                        circle.RemoveAt(takeFrom);
-                        players[p].Add(marble2);
-                        players[p].Sort();
-                        pos = takeFrom;
                         if (verbose) {
-                            Console.WriteLine("[{0}] = SOMETHING INTERESTING HAPPENS (keep {1}, take {2})", p, marble, marble2);
+                            Console.WriteLine("[{0}] = SOMETHING INTERESTING HAPPENS (keep {1}, take {2})", p, marble, takeMarble.Value);
                         }
                     } else {
-                        int insertAt = ((pos + 1) % circle.Count) + 1;
-                        //Console.WriteLine("Insert at: " + insertAt);
-                        circle.Insert(insertAt, marble);
-                        pos = insertAt;
+                        DLNode<int> addedMarble = new DLNode<int>(marble);
+                        current.Next.AddAfter(addedMarble);
+                        current = addedMarble;
                     }
 
                     if (verbose) {
-                        Console.WriteLine("[{0}] = {1}", p, string.Join(' ', circle));
-                        //Console.WriteLine("Current is now: " + circle[pos]);
+                        StringBuilder sb = new StringBuilder();
+                        sb.Append(root.Value);
+                        DLNode<int> n = root.Next;
+                        while(n != root) {
+                            sb.Append(" " + n.Value);
+                            n = n.Next;
+                        }
+                        Console.WriteLine("[{0}] = {1}", p, sb.ToString());
                     }
                 }
-
-                //--
-
-                /*
-                int lookingFor = numPlayers * 23;
-
-                bool test = true;
-                for (int p = 1; p <= numPlayers; p++) {
-                    if (players[p].Count < 2) {
-                        test = false;
-                        break;
-                    }
-
-                    int[] nums = players[p].TakeLast(2).ToArray();
-                    int diff = nums[1] - nums[0];
-                    if(diff != lookingFor) {
-                        test = false;
-                        break;
-                    }
-                }
-
-                if(test) {
-                    break;
-                }
-                */
             }
 
-            Console.WriteLine();
-            Console.WriteLine("{0} players, with {1} marbles.", numPlayers, highMarble);
-            foreach (var player in players) {
-                if (player.Value.Sum() > 0) {
-                    //player.Value.Sort();
-                    Console.WriteLine("[{0}] has {1}", player.Key, string.Join(' ', player.Value));
+            long result = players.Max(p => p.Value.Sum());
+
+            if (result < 50000) {
+                Console.WriteLine();
+                Console.WriteLine("{0} players, with {1} marbles.", numPlayers, highMarble);
+                foreach (var player in players) {
+                    if (player.Value.Sum() > 0) {
+                        player.Value.Sort();
+                        Console.WriteLine("[{0}] has {1}", player.Key, string.Join(' ', player.Value));
+                    }
                 }
+                Console.WriteLine();
             }
-            Console.WriteLine();
-            return players.Max(p => p.Value.Sum());
+
+            return result;
         }
+
+        public class DLNode<T> {
+            public DLNode<T> Previous;
+            public DLNode<T> Next;
+            public T Value;
+
+            public DLNode(T data) {
+                Value = data;
+            }
+
+            public void AddAfter(DLNode<T> after) {
+                DLNode<T> orgNext = Next;
+                Next = after;
+                Next.Previous = this;
+                Next.Next = orgNext;
+                orgNext.Previous = Next;
+            }
+
+            public void AddBefore(DLNode<T> before) {
+                DLNode<T> orgPrev = Previous;
+                Previous = before;
+                Previous.Next = this;
+                Previous.Previous = orgPrev;
+                orgPrev.Next = Previous;
+            }
+        }
+
     }
 }
