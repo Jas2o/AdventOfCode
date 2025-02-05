68817122ff0e21a8226b49d9111e908341a84347
jas2o <43341263+Jas2o@users.noreply.github.com>
2025-02-05 12:04:45 +1100

    2019: Day 1

--- AoC-2019/Day/Day01.cs
+++ AoC-2019/Day/Day01.cs
@@ -3,16 +3,40 @@
     public class Day01
     {
         public static void Run(string file) {
-            Console.WriteLine("Day 1: ???" + Environment.NewLine);
+            Console.WriteLine("Day 1: The Tyranny of the Rocket Equation" + Environment.NewLine);
 
-			string input = File.ReadAllText(file);
 			string[] lines = File.ReadAllLines(file);
+            int[] nums = Array.ConvertAll(lines, int.Parse);
 
-			Console.WriteLine();
-            Console.WriteLine("Part 1: " + 0);
-            //Answer: 
-            Console.WriteLine("Part 2: " + 0);
-            //Answer: 
+            int partA = 0;
+            foreach (int num in nums) {
+                int fuel = (num / 3) - 2;
+                partA += fuel;
+                //Console.WriteLine(fuel);
+            }
+
+            //Console.WriteLine();
+
+            int partB = 0;
+            foreach (int num in nums) {
+                int fuel = (num / 3) - 2;
+                partB += fuel;
+
+                while(true) {
+                    fuel = (fuel / 3) - 2;
+                    if (fuel > 0) {
+                        //Console.WriteLine(fuel);
+                        partB += fuel;
+                    } else
+                        break;
+                }
+            }
+
+			//Console.WriteLine();
+            Console.WriteLine("Part 1: " + partA);
+            //Answer: 3353880
+            Console.WriteLine("Part 2: " + partB);
+            //Answer: 5027950
         }
     }
 }
