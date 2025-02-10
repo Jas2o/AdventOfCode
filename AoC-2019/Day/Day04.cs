921be5bc3a463e8e0c15b34d4442c150c03f8dba
jas2o <43341263+Jas2o@users.noreply.github.com>
2025-02-10 18:04:58 +1100

    2019: Day 4

--- AoC-2019/Day/Day04.cs
+++ AoC-2019/Day/Day04.cs
@@ -1,18 +1,53 @@
-﻿namespace AoC.Day
-{
+﻿namespace AoC.Day {
     public class Day04
     {
         public static void Run(string file) {
-            Console.WriteLine("Day 4: ???" + Environment.NewLine);
+            Console.WriteLine("Day 4: Secure Container" + Environment.NewLine);
 
 			string input = File.ReadAllText(file);
-			string[] lines = File.ReadAllLines(file);
+            if (!input.Contains('-')) {
+                Console.WriteLine("There is no test for this day.");
+                return;
+            }
+            int[] range = Array.ConvertAll(input.Split('-'), int.Parse);
 
-			Console.WriteLine();
-            Console.WriteLine("Part 1: " + 0);
-            //Answer: 
-            Console.WriteLine("Part 2: " + 0);
-            //Answer: 
+            int partA = 0;
+            int partB = 0;
+            for (int n = range[0]; n < range[1]; n++) {
+                string inputN = n.ToString();
+
+                bool hasDouble = false;
+                bool hasDoubleNotPartOfLarger = false;
+                bool hasDecrease = false;
+                int previous = -1;
+
+                for (int i = 0; i < inputN.Length; i++) {
+                    int num = (int)char.GetNumericValue(inputN[i]);
+
+                    if (previous == num) {
+                        hasDouble = true;
+                        string triple = string.Format("{0}{0}{0}", num);
+                        if (!inputN.Contains(triple))
+                            hasDoubleNotPartOfLarger = true;
+                    }
+                    if (num < previous)
+                        hasDecrease = true;
+
+                    previous = num;
+                }
+
+                if (!hasDecrease) {
+                    if(hasDouble)
+                        partA++;
+                    if (hasDoubleNotPartOfLarger)
+                        partB++;
+                }
+            }
+
+            Console.WriteLine("Part 1: " + partA);
+            //Answer: 1729
+            Console.WriteLine("Part 2: " + partB);
+            //Answer: 1172
         }
     }
 }
