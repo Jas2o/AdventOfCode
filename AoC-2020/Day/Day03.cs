c711a9f70d59064eb3cc8615f51bb594158c5d7c
jas2o <43341263+Jas2o@users.noreply.github.com>
2025-03-26 13:03:16 +1100

    2020: Day 3

--- AoC-2020/Day/Day03.cs
+++ AoC-2020/Day/Day03.cs
@@ -3,16 +3,46 @@
     public class Day03
     {
         public static void Run(string file) {
-            Console.WriteLine("Day 3: ???" + Environment.NewLine);
+            Console.WriteLine("Day 3: Toboggan Trajectory" + Environment.NewLine);
 
-			string input = File.ReadAllText(file);
 			string[] lines = File.ReadAllLines(file);
 
-			Console.WriteLine();
-            Console.WriteLine("Part 1: " + 0);
-            //Answer: 
-            Console.WriteLine("Part 2: " + 0);
-            //Answer: 
+            int partA = TestSlope(lines, 3, 1);
+
+            int[] partB_tests = new int[] {
+                TestSlope(lines, 1, 1),
+                partA,
+                TestSlope(lines, 5, 1),
+                TestSlope(lines, 7, 1),
+                TestSlope(lines, 1, 2)
+            };
+            
+            long partB = partB_tests[0];
+            for (int t = 0; t < partB_tests.Length; t++) {
+                Console.WriteLine(partB_tests[t]);
+                if(t > 0)
+                    partB *= partB_tests[t];
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Part 1: " + partA);
+            //Answer: 240
+            Console.WriteLine("Part 2: " + partB);
+            //Answer: 2832009600
+        }
+
+        private static int TestSlope(string[] lines, int r, int d) {
+            int result = 0;
+            int width = lines[0].Length;
+            int right = 0;
+            for (int down = d; down < lines.Length; down += d) {
+                right = (right + r) % width;
+
+                char hit = lines[down][right];
+                if (hit == '#')
+                    result++;
+            }
+            return result;
         }
     }
 }
