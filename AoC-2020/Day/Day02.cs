da4ac6d4561c4e0cbb3ab8c142a12df91f3848e3
jas2o <43341263+Jas2o@users.noreply.github.com>
2025-03-26 12:47:07 +1100

    2020: Day 2

--- AoC-2020/Day/Day02.cs
+++ AoC-2020/Day/Day02.cs
@@ -3,16 +3,51 @@
     public class Day02
     {
         public static void Run(string file) {
-            Console.WriteLine("Day 2: ???" + Environment.NewLine);
+            Console.WriteLine("Day 2: Password Philosophy" + Environment.NewLine);
 
-			string input = File.ReadAllText(file);
 			string[] lines = File.ReadAllLines(file);
 
-			Console.WriteLine();
-            Console.WriteLine("Part 1: " + 0);
-            //Answer: 
-            Console.WriteLine("Part 2: " + 0);
-            //Answer: 
+            int partA = 0;
+            int partB = 0;
+            foreach (string line in lines) {
+                int valid = 0;
+
+                string[] fields = line.Replace(":", "").Split(' ');
+                int[] num = Array.ConvertAll(fields[0].Split('-'), int.Parse);
+
+                int lenBefore = fields[2].Length;
+                string passWithout = fields[2].Replace(fields[1], "");
+                int lenAfter = passWithout.Length;
+                int diff = lenBefore - lenAfter;
+
+                if (diff >= num[0] && diff <= num[1]) {
+                    valid += 1;
+                    partA++;
+                }
+
+                char one = fields[2][num[0] - 1];
+                char two = fields[2][num[1] - 1];
+                char needed = fields[1][0];
+                if (one != two) {
+                    if(one == needed || two == needed) {
+                        valid += 2;
+                        partB++;
+                    }
+                }
+
+                if (valid > 0) {
+                    if (valid == 3)
+                        Console.ForegroundColor = ConsoleColor.Cyan;
+                    Console.WriteLine("{0} = {1}", valid, line);
+                    Console.ResetColor();
+                }
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Part 1: " + partA);
+            //Answer: 445
+            Console.WriteLine("Part 2: " + partB);
+            //Answer: 491
         }
     }
 }
