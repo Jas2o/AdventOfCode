44b133d26ddfca5cef737e0fce9fd216b2658993
jas2o <43341263+Jas2o@users.noreply.github.com>
2025-03-27 14:43:55 +1100

    2020: Day 6

--- AoC-2020/Day/Day06.cs
+++ AoC-2020/Day/Day06.cs
@@ -3,16 +3,50 @@
     public class Day06
     {
         public static void Run(string file) {
-            Console.WriteLine("Day 6: ???" + Environment.NewLine);
+            Console.WriteLine("Day 6: Custom Customs" + Environment.NewLine);
 
-			string input = File.ReadAllText(file);
-			string[] lines = File.ReadAllLines(file);
+            List<string> lines = File.ReadAllLines(file).ToList();
+            lines.Add(string.Empty);
 
-			Console.WriteLine();
-            Console.WriteLine("Part 1: " + 0);
-            //Answer: 
-            Console.WriteLine("Part 2: " + 0);
-            //Answer: 
+            List<Dictionary<char, int>> groups = new List<Dictionary<char, int>>();
+            Dictionary<char, int> inprogress = new Dictionary<char, int>();
+            int count = 0;
+            Dictionary<Dictionary<char, int>, int> groupMemberCount = new Dictionary<Dictionary<char, int>, int>();
+            foreach (string line in lines) {
+                if (line.Length == 0) {
+                    if (inprogress.Any()) {
+                        groups.Add(inprogress);
+                        groupMemberCount.Add(inprogress, count);
+                        inprogress = new Dictionary<char, int>();
+                        count = 0;
+                    }
+                } else {
+                    foreach(char c in line) {
+                        if (inprogress.ContainsKey(c))
+                            inprogress[c]++;
+                        else
+                            inprogress.Add(c, 1);
+                    }
+                    count++;
+                }
+            }
+
+            int partA = 0;
+            int partB = 0;
+            foreach(KeyValuePair<Dictionary<char, int>, int> group in groupMemberCount) {
+                partA += group.Key.Keys.Count();
+                int b = 0;
+                foreach(KeyValuePair<char, int> thing in group.Key) {
+                    if (thing.Value == group.Value)
+                        b++;
+                }
+                partB += b;
+            }
+
+            Console.WriteLine("Part 1: " + partA);
+            //Answer: 6583
+            Console.WriteLine("Part 2: " + partB);
+            //Answer: 3290
         }
     }
 }
