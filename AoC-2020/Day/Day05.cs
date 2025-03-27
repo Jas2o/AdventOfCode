3030a6410732db0d8720b551e6073aa966d009a1
jas2o <43341263+Jas2o@users.noreply.github.com>
2025-03-27 13:59:34 +1100

    2020: Day 5

--- AoC-2020/Day/Day05.cs
+++ AoC-2020/Day/Day05.cs
@@ -3,16 +3,56 @@
     public class Day05
     {
         public static void Run(string file) {
-            Console.WriteLine("Day 5: ???" + Environment.NewLine);
+            Console.WriteLine("Day 5: Binary Boarding" + Environment.NewLine);
 
-			string input = File.ReadAllText(file);
 			string[] lines = File.ReadAllLines(file);
 
-			Console.WriteLine();
-            Console.WriteLine("Part 1: " + 0);
-            //Answer: 
-            Console.WriteLine("Part 2: " + 0);
-            //Answer: 
+            Dictionary<int, string> boardingPasses = new Dictionary<int, string>();
+
+            foreach (string line in lines) {
+                int minRow = 0, maxRow = 127;
+                int minCol = 0, maxCol = 7;
+
+                for (int i = 0; i < line.Length; i++) {
+                    switch (line[i]) {
+                        case 'F':
+                            maxRow = ((maxRow - minRow) / 2) + minRow;
+                            break;
+                        case 'B':
+                            minRow = ((maxRow - minRow) / 2) + 1 + minRow;
+                            break;
+                        case 'L':
+                            maxCol = ((maxCol - minCol) / 2) + minCol;
+                            break;
+                        case 'R':
+                            minCol = ((maxCol - minCol) / 2) + 1 + minCol;
+                            break;
+                    }
+                }
+
+                int id = (minRow * 8) + minCol;
+                boardingPasses.Add(id, string.Format("Seat ID {0} = Row {1}, Col {2}", id, minRow, minCol));
+            }
+
+            boardingPasses = boardingPasses.OrderBy(b => b.Key).ToDictionary();
+
+            int partA = boardingPasses.Max(b => b.Key);
+            int partB = -1;
+            int prev = boardingPasses.Min(b => b.Key) - 1;
+            foreach (KeyValuePair<int, string> boardingPass in boardingPasses) {
+                if (boardingPass.Key > prev + 1) {
+                    partB = prev + 1;
+                    Console.ForegroundColor = ConsoleColor.Cyan;
+                }
+                prev = boardingPass.Key;
+                Console.WriteLine(boardingPass.Value);
+            }
+            Console.ResetColor();
+            Console.WriteLine();
+            Console.WriteLine("Part 1: " + partA);
+            //Answer: 822
+            Console.WriteLine("Part 2: " + partB);
+            //Answer: 705
         }
     }
 }
