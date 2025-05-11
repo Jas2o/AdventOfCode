4a356e15a7bb00870b4af1014ba29506fe9b5696
jas2o <43341263+Jas2o@users.noreply.github.com>
2025-05-11 18:43:15 +1000

    2020: Day 25

--- AoC-2020/Day/Day25.cs
+++ AoC-2020/Day/Day25.cs
@@ -3,16 +3,46 @@
     public class Day25
     {
         public static void Run(string file) {
-            Console.WriteLine("Day 25: ???" + Environment.NewLine);
+            Console.WriteLine("Day 25: Combo Breaker" + Environment.NewLine);
 
-			string input = File.ReadAllText(file);
-			string[] lines = File.ReadAllLines(file);
+            string[] lines = File.ReadAllLines(file);
+            int cardKey = int.Parse(lines[0]);
+            int doorKey = int.Parse(lines[1]);
+            int doorLoop = FindLoop(doorKey);
+            int cardLoop = FindLoop(cardKey);
 
-			Console.WriteLine();
-            Console.WriteLine("Part 1: " + 0);
-            //Answer: 
+            long partA = Transform(cardKey, doorLoop);
+            //long partA = Transform(door, cardLoop);
+
+            Console.WriteLine("Card key: {0}, loop: {1}", cardKey, cardLoop);
+            Console.WriteLine("Door key: {0}, loop: {1}\r\n", doorKey, doorLoop);
+            Console.WriteLine("Part 1: " + partA);
+            //Answer: 7032853
             Console.WriteLine("Part 2: (N/A)");
 			//Answer: (there is no Part 2).
+        }
+
+        public static int FindLoop(int key) {
+            int loop = 0;
+            int subject = 7;
+            long value = 1;
+            while(true) {
+                loop++;
+                value *= subject;
+                value %= 20201227;
+
+                if (value == key)
+                    return loop;
+            }
+        }
+
+        private static long Transform(int subject, int loops) {
+            long value = 1;
+            for (int current = 0; current < loops; current++) {
+                value *= subject;
+                value %= 20201227;
+            }
+            return value;
         }
     }
 }
