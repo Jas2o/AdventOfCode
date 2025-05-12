b0af89494cddf82d64bb003fb01862703a05602b
jas2o <43341263+Jas2o@users.noreply.github.com>
2025-05-12 17:21:08 +1000

    2021: Day 7

--- AoC-2021/Day/Day07.cs
+++ AoC-2021/Day/Day07.cs
@@ -3,16 +3,49 @@
     public class Day07
     {
         public static void Run(string file) {
-            Console.WriteLine("Day 7: ???" + Environment.NewLine);
+            Console.WriteLine("Day 7: The Treachery of Whales" + Environment.NewLine);
 
 			string input = File.ReadAllText(file);
-			string[] lines = File.ReadAllLines(file);
+            int[] crabs = Array.ConvertAll(input.Split(','), int.Parse);
+            int min = crabs.Min();
+            int max = crabs.Max();
 
-			Console.WriteLine();
-            Console.WriteLine("Part 1: " + 0);
-            //Answer: 
-            Console.WriteLine("Part 2: " + 0);
-            //Answer: 
+            int partA = int.MaxValue;
+            int partB = int.MaxValue;
+            int partA_dest = 0;
+            int partB_dest = 0;
+
+            for (int dest = min; dest <= max; dest++) {
+                int fuelA = 0;
+                int fuelB = 0;
+                foreach (int crab in crabs) {
+                    int diff = Math.Abs(dest - crab);
+                    fuelA += diff;
+                    if (fuelB != int.MaxValue) {
+                        for (int d = 1; d <= diff; d++)
+                            fuelB += d;
+                        if (fuelB > partB) {
+                            //Skip the rest of adding to B
+                            fuelB = int.MaxValue;
+                        }
+                    }
+                }
+
+                if (fuelA < partA) {
+                    partA = fuelA;
+                    partA_dest = dest;
+                }
+
+                if (fuelB < partB) {
+                    partB = fuelB;
+                    partB_dest = dest;
+                }
+            }
+
+            Console.WriteLine("Part 1: {0} (at {1})", partA, partA_dest);
+            //Answer: 341558 (at 371)
+            Console.WriteLine("Part 2: {0} (at {1})", partB, partB_dest);
+            //Answer: 93214037 (at 484)
         }
     }
 }
