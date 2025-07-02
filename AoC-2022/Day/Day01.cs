05516042784e87d1efb111142ae5dff5b57018cf
jas2o <43341263+Jas2o@users.noreply.github.com>
2025-07-02 17:12:37 +1000

    2022: Day 1

--- AoC-2022/Day/Day01.cs
+++ AoC-2022/Day/Day01.cs
@@ -3,16 +3,32 @@
     public class Day01
     {
         public static void Run(string file) {
-            Console.WriteLine("Day 1: ???" + Environment.NewLine);
+            Console.WriteLine("Day 1: Calorie Counting" + Environment.NewLine);
 
-			string input = File.ReadAllText(file);
-			string[] lines = File.ReadAllLines(file);
+            string[] lines = File.ReadAllLines(file);
 
-			Console.WriteLine();
-            Console.WriteLine("Part 1: " + 0);
-            //Answer: 
-            Console.WriteLine("Part 2: " + 0);
-            //Answer: 
+            int total = 0;
+            List<int> totals = new List<int>();
+            List<string> listLines = new List<string>(lines);
+            listLines.Add("");
+
+            foreach (string line in listLines) {
+                if (line.Length == 0) {
+                    Console.WriteLine(total);
+                    totals.Add(total);
+                    total = 0;
+                } else
+                    total += int.Parse(line);
+            }
+
+            int partA = totals.Max();
+            int partB = totals.OrderDescending().Take(3).Sum();
+
+            Console.WriteLine();
+            Console.WriteLine("Part 1: " + partA);
+            //Answer: 67658
+            Console.WriteLine("Part 2: " + partB);
+            //Answer: 200158
         }
     }
 }
