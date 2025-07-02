8d622aed1140efd6fb60afe7c97e57e1a0fa846c
jas2o <43341263+Jas2o@users.noreply.github.com>
2025-07-02 22:24:26 +1000

    2022: Day 4

--- AoC-2022/Day/Day04.cs
+++ AoC-2022/Day/Day04.cs
@@ -3,16 +3,34 @@
     public class Day04
     {
         public static void Run(string file) {
-            Console.WriteLine("Day 4: ???" + Environment.NewLine);
+            Console.WriteLine("Day 4: Camp Cleanup" + Environment.NewLine);
 
-			string input = File.ReadAllText(file);
 			string[] lines = File.ReadAllLines(file);
 
-			Console.WriteLine();
-            Console.WriteLine("Part 1: " + 0);
-            //Answer: 
-            Console.WriteLine("Part 2: " + 0);
-            //Answer: 
+            int partA = 0;
+            int partB = 0;
+            foreach (string line in lines) {
+                int[] nums = Array.ConvertAll(line.Replace(',', '-').Split('-'), int.Parse);
+
+                //One side fully contains other
+                bool test1 = (nums[0] <= nums[2]);
+                bool test2 = (nums[1] >= nums[3]);
+                bool test3 = (nums[1] <= nums[3]);
+                bool test4 = (nums[0] >= nums[2]);
+                if ((test1 && test2) || (test3 && test4))
+                    partA++;
+
+                //Any overlap
+                bool test5 = (nums[1] >= nums[2]);
+                bool test6 = (nums[3] >= nums[0]);
+                if (test5 && test6)
+                    partB++;
+            }
+
+            Console.WriteLine("Part 1: " + partA);
+            //Answer: 532
+            Console.WriteLine("Part 2: " + partB);
+            //Answer: 854
         }
     }
 }
