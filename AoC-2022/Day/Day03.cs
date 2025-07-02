694e4505a96e728d027c3a585832a4465023ed1c
jas2o <43341263+Jas2o@users.noreply.github.com>
2025-07-02 20:29:49 +1000

    2022: Day 3

--- AoC-2022/Day/Day03.cs
+++ AoC-2022/Day/Day03.cs
@@ -3,16 +3,51 @@
     public class Day03
     {
         public static void Run(string file) {
-            Console.WriteLine("Day 3: ???" + Environment.NewLine);
+            Console.WriteLine("Day 3: Rucksack Reorganization" + Environment.NewLine);
 
-			string input = File.ReadAllText(file);
-			string[] lines = File.ReadAllLines(file);
+            string[] lines = File.ReadAllLines(file);
 
-			Console.WriteLine();
-            Console.WriteLine("Part 1: " + 0);
-            //Answer: 
-            Console.WriteLine("Part 2: " + 0);
-            //Answer: 
+            int count = 0;
+            int partA = 0;
+            foreach(string line in lines) {
+                char[] left = line.Substring(0, line.Length / 2).ToCharArray();
+                char[] right = line.Substring(line.Length / 2).ToCharArray();
+
+                IEnumerable<char> both = left.Intersect(right);
+                foreach (char c in both) {
+                    int num = c - 96;
+                    if (num <= 0)
+                        num += 58;
+                    partA += num;
+                    Console.Write("{0}:{1,-2} ", c, num);
+                    count++;
+                    if (count % 16 == 0)
+                        Console.WriteLine();
+                }
+            }
+            Console.WriteLine("\r\n");
+
+            int partB = 0;
+            for(int i = 0; i < lines.Length; i += 3) {
+                char[] line0 = lines[i].ToCharArray();
+                char[] line1 = lines[i + 1].ToCharArray();
+                char[] line2 = lines[i + 2].ToCharArray();
+
+                var badge = line0.Intersect(line1).Intersect(line2);
+                foreach (char c in badge) {
+                    Console.Write(c);
+                    int num = c - 96;
+                    if (num <= 0)
+                        num += 58;
+                    partB += num;
+                }
+            }
+            Console.WriteLine("\r\n");
+
+            Console.WriteLine("Part 1: " + partA);
+            //Answer: 7826
+            Console.WriteLine("Part 2: " + partB);
+            //Answer: 2577
         }
     }
 }
