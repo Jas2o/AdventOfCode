32b3bfa626a430bdfe37c39a84bbba8ed01119b2
jas2o <43341263+Jas2o@users.noreply.github.com>
2025-07-03 12:01:52 +1000

    2022: Day 6

--- AoC-2022/Day/Day06.cs
+++ AoC-2022/Day/Day06.cs
@@ -3,16 +3,38 @@
     public class Day06
     {
         public static void Run(string file) {
-            Console.WriteLine("Day 6: ???" + Environment.NewLine);
+            Console.WriteLine("Day 6: Tuning Trouble");
 
-			string input = File.ReadAllText(file);
 			string[] lines = File.ReadAllLines(file);
+            foreach (string line in lines) {
+                //There's only one line in the real input.
+                Console.WriteLine();
 
-			Console.WriteLine();
-            Console.WriteLine("Part 1: " + 0);
-            //Answer: 
-            Console.WriteLine("Part 2: " + 0);
-            //Answer: 
+                int partA = -1;
+                int partB = -1;
+                int start = -1;
+                while(start < line.Length) {
+                    start++;
+                    if (partA == -1) {
+                        IEnumerable<char> window = line.Substring(start, 4).ToCharArray().Distinct();
+                        if (window.Count() == 4)
+                            partA = start + 4;
+                    }
+
+                    if (partB == -1) {
+                        IEnumerable<char> window = line.Substring(start, 14).ToCharArray().Distinct();
+                        if (window.Count() == 14) {
+                            partB = start + 14;
+                            break;
+                        }
+                    }
+                }
+
+                Console.WriteLine("Part 1: " + partA);
+                //Answer: 1850
+                Console.WriteLine("Part 2: " + partB);
+                //Answer: 2823
+            }
         }
     }
 }
