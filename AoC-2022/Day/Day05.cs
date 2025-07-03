a3c8e7b5c5819845f0938fcc1997cef3f71f0f77
jas2o <43341263+Jas2o@users.noreply.github.com>
2025-07-03 11:44:29 +1000

    2022: Day 5

--- AoC-2022/Day/Day05.cs
+++ AoC-2022/Day/Day05.cs
@@ -3,16 +3,74 @@
     public class Day05
     {
         public static void Run(string file) {
-            Console.WriteLine("Day 5: ???" + Environment.NewLine);
+            Console.WriteLine("Day 5: Supply Stacks" + Environment.NewLine);
 
-			string input = File.ReadAllText(file);
 			string[] lines = File.ReadAllLines(file);
 
+            //Setup
+            int iBlank = Array.IndexOf(lines, "");
+            int[] nums = Array.ConvertAll(lines[iBlank - 1].Split(' ', StringSplitOptions.RemoveEmptyEntries), int.Parse);
+            Dictionary<int, Stack<char>> stacksA = new Dictionary<int, Stack<char>>();
+            Dictionary<int, Stack<char>> stacksB = new Dictionary<int, Stack<char>>();
+            foreach (int n in nums) {
+                stacksA[n] = new Stack<char>();
+                stacksB[n] = new Stack<char>();
+            }
+            for (int i = iBlank - 2; i >= 0; i--) {
+                string line = lines[i];
+                int pos = 0;
+                for(int k = 1; k < line.Length; k += 4) {
+                    pos++;
+                    char c = line[k];
+                    if (c != ' ') {
+                        stacksA[pos].Push(c);
+                        stacksB[pos].Push(c);
+                    }
+                }
+            }
+
+            DisplayAndGetTops(stacksA, nums, "Before:");
+
+            //Do moves
+            for (int i = iBlank + 1; i < lines.Length; i++) {
+                string[] fields = lines[i].Split(' ');
+                int count = int.Parse(fields[1]);
+                int stackSource = int.Parse(fields[3]);
+                int stackDest = int.Parse(fields[5]);
+
+                char[] moveB = stacksB[stackSource].Take(count).Reverse().ToArray();
+                for (int current = 0; current < count; current++) {
+                    char c = stacksA[stackSource].Pop();
+                    stacksA[stackDest].Push(c);
+
+                    stacksB[stackSource].Pop();
+                    stacksB[stackDest].Push(moveB[current]);
+                }
+            }
+
+            string partA = DisplayAndGetTops(stacksA, nums, "Part 1:");
+            string partB = DisplayAndGetTops(stacksB, nums, "Part 2:");
+
 			Console.WriteLine();
-            Console.WriteLine("Part 1: " + 0);
-            //Answer: 
-            Console.WriteLine("Part 2: " + 0);
-            //Answer: 
+            Console.WriteLine("Part 1: " + partA);
+            //Answer: ZSQVCCJLL
+            Console.WriteLine("Part 2: " + partB);
+            //Answer: QZFJRWHGS
+        }
+
+        private static string DisplayAndGetTops(Dictionary<int, Stack<char>> stacks, int[] nums, string heading) {
+            List<char> top = new List<char>();
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(heading);
+            Console.ResetColor();
+            foreach (int n in nums) {
+                string str = string.Join(',', stacks[n].Reverse());
+                Console.WriteLine("{0} => {1}", n, str);
+                top.Add(stacks[n].Peek());
+            }
+            Console.WriteLine();
+
+            return new string(top.ToArray());
         }
     }
 }
